e228288580e584dfbec0d9ef75ad6248ec1c9d87
cristhiansosa90 <cristhian.jsosa@gmail.com>
2019-08-24 09:05:05 -0500

    Creado proyecto

---
+++ app/src/main/java/com/example/myapplication/Ejercicio5.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Ejercicio5 extends AppCompatActivity {
+ Button btn_1,btn_2,btn_3,btn_4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ejercicio5);
+    }
+}
