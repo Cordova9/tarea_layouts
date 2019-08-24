e6aa3635a7f305eecdd825ef9641ab6ab0e90675
Cordova9 <charliecrdv@gmail.com>
2019-08-24 10:52:03 -0500

    csdvcad

--- app/src/main/java/com/example/myapplication/Ejercicio6.java
+++ app/src/main/java/com/example/myapplication/Ejercicio6.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Ejercicio6 extends AppCompatActivity {
 
